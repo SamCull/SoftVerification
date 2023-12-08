f5c867c01e640b9de11f505b3f730d45e8285d17
Sam Cullen <samcull2000@gmail.com>
2023-12-08 14:18:25 +0000

    Implemented Test for Staff kind

--- Rate.java
+++ Rate.java
@@ -36,8 +36,8 @@ public class Rate {
         if (this.kind == CarParkKind.VISITOR && (!isValidPeriods(reducedPeriods) || !isValidPeriods(normalPeriods) || !isValidPeriods(reducedPeriods, normalPeriods))) {
             throw new IllegalArgumentException("The periods overlap for VISITOR rate");
         }
-        if (this.kind == CarParkKind.STUDENT && (!isValidPeriods(reducedPeriods) || !isValidPeriods(normalPeriods) || !isValidPeriods(reducedPeriods, normalPeriods))) {
-            throw new IllegalArgumentException("The periods overlap for STUDENT rate");
+        if (this.kind == CarParkKind.STAFF && (!isValidPeriods(reducedPeriods) || !isValidPeriods(normalPeriods) || !isValidPeriods(reducedPeriods, normalPeriods))) {
+            throw new IllegalArgumentException("The periods overlap for STAFF rate");
         }
 
 
