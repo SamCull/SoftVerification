e1efc99da03bc7d2102aa5fb291ab031279fbff2
Sam Cullen <samcull2000@gmail.com>
2023-12-08 14:10:48 +0000

    Added calculate with Visitor kind

--- CullenSamTestTask3.java
+++ CullenSamTestTask3.java
@@ -230,7 +230,14 @@ public class CullenSamTestTask3 {
         Period studentStay = new Period(4, 14);
         assertEquals(new BigDecimal(16), studentRate.calculate(studentStay));//14
     }
-
+    @Test
+    void testCalculateWithVisitorKind() {
+        Rate visitorRate = new Rate(CarParkKind.VISITOR, new BigDecimal(5), new BigDecimal(2),
+                new ArrayList<>(Arrays.asList(new Period(2, 5), new Period(10, 12))),
+                new ArrayList<>(Arrays.asList(new Period(6, 8), new Period(14, 16))));
+        Period visitorStay = new Period(1, 6);
+        assertEquals(new BigDecimal(15), visitorRate.calculate(visitorStay));//0
+    }
 
 
 }
